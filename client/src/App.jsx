9107db254ad29a067947697423249893deee8719
sanjkthakur18 <sanjkthakur18@gmail.com>
2024-08-04 16:33:09 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import './App.css'
 
 const App = () => {
   const { authenticated } = useUserContext()
+  const role = localStorage.getItem('role')
 
   const adminAuthenticated = authenticated && role === 'admin'
   const executiveAuthenticated = authenticated && role === 'executive'
